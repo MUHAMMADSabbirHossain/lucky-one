d23b0346bdd40c4bd167a5e5cd8cbe0307b047e4
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-06 18:09:19 +0600

    cart detail styled none

--- src/Components/AnimeShopListCart/AnimeShopLIstCart.js
+++ src/Components/AnimeShopListCart/AnimeShopLIstCart.js
@@ -6,7 +6,7 @@ const AnimeShopLIstCart = ({ anime }) => {
     // console.log(anime.images.jpg.image_url);
 
     return (
-        <div>
+        <div className='anime-shop-list-cart'>
             <div>
                 <h3>{anime.title}</h3>
             </div>
@@ -17,7 +17,7 @@ const AnimeShopLIstCart = ({ anime }) => {
                 <p>Rating: {anime.rating}</p>
                 <p>Episodes: {anime.episodes}</p>
                 <p>Duration: {anime.duration}</p>
-                {/* <p>Score: {anime.background}</p> */}
+                <p className='anime-shop-list-cart-detail'>Score: {anime.background}</p>
             </div>
         </div>
     );
