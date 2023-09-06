9873f15092944e0a71872f0b59c87091450172e7
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-06 12:08:14 +0600

    nav section added and styled.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,16 @@ import "./Header.css";
 const Header = () => {
     return (
         <div>
+            <nav>
+                <div><h2>Logo</h2></div>
+                <h1>Anime Website!!!</h1>
+                <div id='nav-menu'>
+                    <a href="">Home</a>
+                    <a href="">Category</a>
+                    <a href="">API's Links</a>
+                    <a href="">About Us</a>
+                </div>
+            </nav>
         </div>
     );
 };
