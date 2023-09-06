410a4dad7ebf60bb7241af9a31714159453637d5
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-06 17:53:56 +0600

    animeshopcart styled for position sticky.

--- src/Components/AnimeShopCart/AnimeShopCart.js
+++ src/Components/AnimeShopCart/AnimeShopCart.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./AnimeShopCart.css";
 
 const AnimeShopCart = () => {
     return (
-        <div>
-            <h2>Watch Summary</h2>
-            <h5></h5>
-            <button>Clear History</button>
+        <div id='anime-shop-cart'>
+            <div>
+                <h2>Watch Summary</h2>
+                <h5></h5>
+                <button>Clear History</button>
+            </div>
         </div>
     );
 };
